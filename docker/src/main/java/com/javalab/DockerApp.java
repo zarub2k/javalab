ee763dffbf2cbfd812f835bc705cfd7885f3e259
Tham <zarub2k@gmail.com>
2024-12-08 00:24:58 +0530

    docker module tried

---
+++ docker/src/main/java/com/javalab/DockerApp.java
@@ -0,0 +1,10 @@
+package com.javalab;
+
+/**
+ * @author tham
+ */
+public class DockerApp {
+    public static void main(String[] args) {
+        System.out.println("Docker app module");
+    }
+}
