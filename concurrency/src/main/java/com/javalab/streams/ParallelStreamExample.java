7fc607acf5e418acdf88b9a791f0dcc065fd9f1c
Tham <zarub2k@gmail.com>
2024-12-09 23:30:34 +0530

    concurrency module is created

---
+++ concurrency/src/main/java/com/javalab/streams/ParallelStreamExample.java
@@ -0,0 +1,18 @@
+package com.javalab.streams;
+
+/**
+ * @author tham
+ */
+public class ParallelStreamExample {
+    public static void main(String[] args) {
+        System.out.println("Parallel stream example");
+    }
+
+    private void doNormalStream() {
+
+    }
+
+    private void doParallelStream() {
+        
+    }
+}
