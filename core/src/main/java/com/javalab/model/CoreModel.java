1777a78eb9adfefe7298cd1296c6bbac361facea
Tham <zarub2k@gmail.com>
2024-12-09 11:18:47 +0530

    Core module is created

---
+++ core/src/main/java/com/javalab/model/CoreModel.java
@@ -0,0 +1,17 @@
+package com.javalab.model;
+
+public class CoreModel {
+    private String name;
+
+    public CoreModel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ functional/src/main/java/com/javalab/CoreModelUsage.java
@@ -0,0 +1,10 @@
+package com.javalab;
+
+import com.javalab.model.CoreModel;
+
+public class CoreModelUsage {
+    public static void main(String[] args) {
+        CoreModel coreModel = new CoreModel("Tham");
+        System.out.println(coreModel.getName());
+    }
+}
