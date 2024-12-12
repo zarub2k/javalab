02b0d40ba475ad7c18ab3e0e0d935c2a674a8eb5
Tham <zarub2k@gmail.com>
2024-12-13 00:05:07 +0530

    Immutable list work is done

--- functional/src/main/java/com/javalab/immutable/ImmutableList.java
+++ functional/src/main/java/com/javalab/immutable/ImmutableList.java
@@ -17,6 +17,33 @@ public class ImmutableList {
     }
 
     private void doCopiesOf() {
+        System.out.println("*** doCopiesOf() ***");
+        List<Integer> numbers = new ArrayList<>(8);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        List<Integer> unmodifiable = List.copyOf(numbers);
+        System.out.println("Initial size: " + unmodifiable.size());
+
+        numbers.add(4);
+        System.out.println("After size: " + unmodifiable.size());
+
+        //Size doesn't change even if you add elements in the original list
+        //Direct modification is not allowed in the copied list
+    }
+
+    private void doFactory() {
+        System.out.println("*** doFactory() ***");
+        List<Integer> numbers = List.of(1, 2, 3);
+        System.out.println(numbers.size());
+
+        //Modification is not allowed
+    }
+
+    private void doUnmodifiableList() {
+        System.out.println("*** doUnmodifiableList() ***");
+
         List<Integer> numbers = new ArrayList<>(8);
         numbers.add(1);
         numbers.add(2);
@@ -30,12 +57,5 @@ public class ImmutableList {
         System.out.println(unmodifiable.size());
 
         //direct modification of unmodifiable is not allowed
-    }
-
-    private void doFactory() {
-
-    }
-
-    private void doUnmodifiableList() {
     }
 }
