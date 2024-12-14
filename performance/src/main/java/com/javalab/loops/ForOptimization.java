4280b3ae2e6f419c7f51e40f8eda911cfa0ca7f5
Tham <zarub2k@gmail.com>
2024-12-15 01:02:43 +0530

    For optimization work started

---
+++ performance/src/main/java/com/javalab/loops/ForOptimization.java
@@ -0,0 +1,50 @@
+package com.javalab.loops;
+
+import com.javalab.model.Book;
+import com.javalab.model.BooksGenerator;
+import com.javalab.util.StopWatch;
+
+import java.util.List;
+
+/**
+ * @author Tham
+ */
+public class ForOptimization {
+    public static void main(String[] args) {
+        ForOptimization forOptimization = new ForOptimization();
+        forOptimization.doNormal();
+        forOptimization.doOptimize();
+    }
+
+    private void doNormal() {
+        List<Book> books = BooksGenerator.books();
+        int count = 0;
+
+        StopWatch stopWatch = StopWatch.start();
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).title().contains("e")) {
+                count++;
+            }
+        }
+
+        System.out.println("Time taken for doNormal(): " + stopWatch.time());
+        System.out.println("Number of books matched: " + count);
+    }
+
+    private void doOptimize() {
+        List<Book> books = BooksGenerator.books();
+        int count = 0;
+
+        int size = books.size();
+        StopWatch stopWatch = StopWatch.start();
+        for (int i = 0; i < size; i++) {
+            String title = books.get(i).title();
+            if (title.indexOf('e') != -1) {
+                count++;
+            }
+        }
+
+        System.out.println("Time taken for doOptimize(): " + stopWatch.time());
+        System.out.println("Number of books matched... " + count);
+    }
+}
