af9b818a5541f6a4ecfd311ba790443b23060f4d
Tham <zarub2k@gmail.com>
2024-12-21 13:37:19 +0530

    Java 21 assessment work continued

--- ocp-java21/src/main/java/com/javalab/ocpjava21/assessment/Assessment.java
+++ ocp-java21/src/main/java/com/javalab/ocpjava21/assessment/Assessment.java
@@ -1,11 +1,66 @@
 package com.javalab.ocpjava21.assessment;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @author Tham
  *
  * Refer: <a href="https://learning.oreilly.com/library/view/ocp-oracle-certified/9781394286614/f08.xhtml#fintro-sec-0552">...</a>
  */
 public class Assessment {
+
+
+    /**
+     * Result -> 15
+     * No matter what the other method does, it will not change the value of a
+     * Because the callee always sends the copy of the variable
+     * The addToInt method can't change the value of a
+     */
+    public void do05() {
+        var a = 15;
+        var b = 10;
+        Assessment.addToInt(a, b);
+        System.out.println(a);
+    }
+    private static void addToInt(int a, int amountToAdd) {
+        a = a + amountToAdd % -5;
+    }
+
+    /**
+     * Result -> The code doesn't terminate
+     * The filter will check for non-empty. Nothing will pass through the filter
+     * So the code will never terminate
+     */
+    public void do04() {
+        Predicate<String> empty = String::isEmpty;
+        Predicate<String> nonEmpty = empty.negate();
+
+        var result = Stream.generate(() -> "")
+                .filter(nonEmpty)
+                .collect(Collectors.groupingBy(k -> k))
+                .entrySet()
+                .stream()
+                .map(Map.Entry::getValue)
+                .flatMap(Collection::stream)
+                .collect(Collectors.partitioningBy(nonEmpty));
+        System.out.println(result);
+    }
+
+    /**
+     * validate the cross type assignment of int / float / double
+     */
+    public void do02() {
+        int moon = 9, star = 2 + 2 * 3;
+        float sun = star > 10 ? 1 : 3;
+        double jupiter = (sun + moon) - 1.0f;
+        int mars = --moon <= 8 ? 2 : 3;
+        System.out.println(sun + ", " + jupiter + ", " + mars);
+    }
+
     public void do01() {
         final int score1 = 8, score2 = 3;
         Integer myScore = 7;
--- ocp-java21/src/test/java/com/javalab/ocpjava21/assessment/AssessmentTest.java
+++ ocp-java21/src/test/java/com/javalab/ocpjava21/assessment/AssessmentTest.java
@@ -9,6 +9,21 @@ public class AssessmentTest {
     Assessment assessment = new Assessment();
 
     @Test
+    public void testDo05() {
+        assessment.do05();
+    }
+
+    @Test
+    public void testDo04() {
+        assessment.do04();
+    }
+
+    @Test
+    public void testDo02() {
+        assessment.do02();
+    }
+
+    @Test
     public void testDo01() {
         assessment.do01();
     }
