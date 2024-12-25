0bdfcfb7322ea0490f3fc12f09cbd855a3f68f13
Tham <zarub2k@gmail.com>
2024-12-25 19:43:37 +0530

    switch example tried

--- basic/src/main/java/com/javalab/basic/controlflow/SwitchExample.java
+++ basic/src/main/java/com/javalab/basic/controlflow/SwitchExample.java
@@ -4,11 +4,37 @@ package com.javalab.basic.controlflow;
  * @author Tham
  */
 public class SwitchExample {
-    private void switchExpression() {
+    enum Shape {Circle, Square, Triangle}
 
+    public void switchExpression(Shape shape) {
+        String shapeName = switch (shape) {
+            case Square -> {
+                yield "This is a square";
+            }
+            case Circle -> "This is a square";
+            case Triangle -> "This is a triangle";
+            case null -> throw new IllegalArgumentException("Not a proper shape");
+        };
+
+        System.out.println(shapeName);
     }
 
-    private void normalSwitch() {
+    public void normalSwitch(Shape shape) {
+        String shapeName = "";
+        switch (shape) {
+            case Circle:
+                shapeName = "This is a circle";
+                break;
+            case Square:
+                shapeName = "This is a square";
+                break;
+            case Triangle:
+                shapeName = "This is a triangle";
+                break;
+            case null:
+                System.out.println("Bad shape");
+        }
 
+        System.out.println(shapeName);
     }
 }
--- basic/src/test/java/com/javalab/basic/controlflow/SwitchExampleTest.java
+++ basic/src/test/java/com/javalab/basic/controlflow/SwitchExampleTest.java
@@ -6,14 +6,17 @@ import org.junit.jupiter.api.Test;
  * @author Tham
  */
 public class SwitchExampleTest {
+    SwitchExample instance = new SwitchExample();
 
     @Test
     public void testSwitchExpression() {
-
+        instance.switchExpression(SwitchExample.Shape.Square);
+        instance.switchExpression(null);
     }
 
     @Test
     public void testNormalSwtich() {
-
+        instance.normalSwitch(SwitchExample.Shape.Square);
+        instance.normalSwitch(null);
     }
 }
