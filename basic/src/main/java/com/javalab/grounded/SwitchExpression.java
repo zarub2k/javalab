615dc5492c3ad7be04921b5aa4e8dcfd6779dc78
Tham <zarub2k@gmail.com>
2025-05-12 23:42:37 +0530

    sample code is added

---
+++ basic/src/main/java/com/javalab/grounded/SwitchExpression.java
@@ -0,0 +1,11 @@
+package com.javalab.grounded;
+
+/**
+ *
+ * @author Tham
+ */
+public class SwitchExpression {
+    public static void main(String[] args) {
+        System.out.println("xx");
+    }
+}
