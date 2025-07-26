7badd7de1a25cf293196b5dd6a05fce38038c670
Tham <zarub2k@gmail.com>
2025-07-26 22:11:16 +0530

    Divisors method is added

--- ocp-java21/src/main/java/com/javalab/ocpjava21/challenges/MathChallenge.java
+++ ocp-java21/src/main/java/com/javalab/ocpjava21/challenges/MathChallenge.java
@@ -10,9 +10,33 @@ import java.util.List;
  */
 public class MathChallenge {
     public static void main(String[] args) {
-        MathChallenge.extractDigits(2710);
+        MathChallenge.findDivisors(275);
     }
     
+    /**
+     * Find the divisors of a given integer value
+     * 
+     * @param value 
+     */
+    public static void findDivisors(int value) {
+        //Validate if the input is zero or negative number
+        if (value <= 0) {
+            System.out.println("Provide a value which is greater than 0");
+            return;
+        }
+        
+        final List<Integer> divisors = new ArrayList();
+        for (int i = 1; i <= value / 2; i++) {
+            if (value % i == 0) {
+                divisors.add(i);
+            }
+        }
+        
+        //If needed add the same number itself
+        divisors.add(value);
+        
+        System.out.println("Divisors are: \n" + divisors);
+    }
     
     public static void extractDigits(int value) {
         List<Integer> finalNumbers = new ArrayList();
